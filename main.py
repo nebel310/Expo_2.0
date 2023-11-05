eef10ca49800c27361910b03144bb007b8013e53
Vlados <89387622+nebel310@users.noreply.github.com>
2023-11-05 23:34:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,37 @@ from config import open_weather_token
 import requests
 from pprint import pprint
 import datetime
+from num2words import num2words
+import subprocess
+import webbrowser
+from playsound import playsound
+import random
+
 
 
 
 
 def greeting():
-    speak('привет')
+    speak('привет.')
 
 
 def goodbye():
-    speak('до свидания')
+    speak('до свидания.')
 
 
 def help():
-    speak('я умею показывать погоду, считать, писать и переводить, как чи+сла, так, и буквы')
+    speak('я умею показывать погоду, считать, писать и переводить, как чи+сла, так, и буквы, а также выполнять простые действия с вашим компьютером.')
 
 
 
 
-def weather(city, open_weather_token, command):
+def thank():
+    speak("Всегда пожалуйста.")
+
+
+
+
+def weather(city, open_weather_token):
 
 
     code_to_status = {
@@ -59,39 +71,16 @@ def weather(city, open_weather_token, command):
         else:
             ws = "я не могу понять погоду. посмотрите в окно."
         
-        speak("Что вам подсказать по погоде?")
-        if 'полная сводка' in listen():
-            text = f"В городе москве сейчас:, {int(cur_weather)} градусов, но ощущается как {int(feels_like)}."
+        print('cheked: weather')
+        text = f"В городе москве сейчас:, {num2words(int(cur_weather), lang='ru')} градусов по ц+е+льсию, но ощущается как {num2words(int(feels_like), lang='ru')}."
 
-            if code_to_status["Clouds"] or code_to_status["Clear"]:
-                text += ' Погода {ws}.'
-            elif code_to_status["Rain"] or code_to_status["Drizzle"] or code_to_status["Thunderstorm"]:
-                text += ' На улице {ws}.'
+        if ws == code_to_status["Clouds"] or ws == code_to_status["Clear"]:
+            text += f' Погода {ws}.'
+        elif code_to_status["Rain"] or code_to_status["Drizzle"] or code_to_status["Thunderstorm"]:
+            text += f' На улице {ws}.'
 
-            text += f' Скорость ветра {int(wind)} метров в секунду. Давление {pressure} паскаль.'
-            speak(text)
-
-        elif 'сколько градусов' in listen():
-            text = f"В городе москве сейчас:, {int(cur_weather)} градусов, но ощущается как {int(feels_like)}."
-
-            if code_to_status["Clouds"] or code_to_status["Clear"]:
-                text += ' Погода {ws}.'
-            elif code_to_status["Rain"] or code_to_status["Drizzle"] or code_to_status["Thunderstorm"]:
-                text += ' На улице {ws}.'
-            
-            speak(text)
-        
-        elif 'ветер' in listen() or 'давление' in listen():
-            text = f'Скорость ветра {int(wind)} метров в секунду. Давление {pressure} паскаль.'
-            speak(text)
-        
-        elif 'когда' in listen() or 'закат' in listen() or 'рассвет' in listen():
-            text = f"Рассвет {sunrise_timestamp}, а закат {sunset_timestamp}"
-            speak(text)
-        
-        else:
-            pass
-            #УСЛОВИЕ ВЫХОДА ИЗ ФУНКЦИИ
+        text += f" Скорость ветра {num2words(int(wind), lang='ru')} метров в секунду. Давление {num2words(int(pressure), lang='ru')} паскаль."
+        speak(text)
 
 
     except Exception as ex:
@@ -102,35 +91,84 @@ def weather(city, open_weather_token, command):
 
 
 def open_browser():
-    speak('команда в разработке')
+    path_to_exe = "C:/Users/vladg/AppData/Local/Yandex/YandexBrowser/Application/browser.exe"
+    subprocess.Popen(path_to_exe)
+
+
+def open_sites():
+    speak("Какой сайт открыть?")
+    search = listen()
+    # search = str(input("Сайт: "))
+
+    if 'мэш' in search or 'меж' in search or 'мэж' in search:
+        webbrowser.open('https://dnevnik.mos.ru/diary/schedules', new=0)
+
+    elif 'ютуб' in search:
+        webbrowser.open('https://youtube.com', new=0)
+
+    elif 'поисковик' in search or 'яндекс' in search:
+        webbrowser.open('https://yandex.com', new=0)
+    
+    elif 'переводчик' in search:
+        webbrowser.open('https://translate.yandex.ru/', new=0)
+    
+    elif 'перплексити' in search or 'чат' in search or 'чат джипити' in search:
+        webbrowser.open('https://www.perplexity.ai/', new=0)
+    
+    elif 'сборник' in search:
+        webbrowser.open('https://vk.com/doc3619616_672138766?hash=lMtTIuayMdBoxRCGj2dinguhNOWEcyD5zByEVv01I8o', new=0)
+    
+    elif 'математика' in search:
+        webbrowser.open('https://app.idroo.com/boards/PsMhS6XVtj', new=0)
+        webbrowser.open('https://mathb-ege.sdamgia.ru/', new=0)
+    
+    else:
+        speak("Это сложно. Поискать в интернете?")
+
+        if 'да' in listen() or 'давай' in listen():
+            speak(f"Вот, что мне удалось найти по запросу, {search}.")
+            url = f"https://www.yandex.com/search?text={search}"
+            webbrowser.open(url, new=0)
+
+        else:
+            print("exit")
+
+
 
 
 def calc():
-    speak('команда в разработке')
+    speak('команда в разработке.')
+
+
 
 
 def convert():
-    speak('команда в разработке')
+    speak('команда в разработке.')
+
+
 
 
 def translate():
-    speak('команда в разработке')
+    speak('команда в разработке.')
 
 
 
 
 def main():
     city = 'moscow'
-    speak(f'Здравствуйте, я ваш виртуальный друг, {config.SP_NAME}.')
+    # speak(f'Здравствуйте, я ваш виртуальный друг, {config.SP_NAME}.')
+    speak(f'Чпокс.')
 
     while True:
         user_command = listen()
+        # Для теста:
+        # user_command = str(input("Команда: "))
         try:
             execute_command(user_command)
         except CommandNotFoundException:
             print('команда не найдена')
         except Exception as e:
-            speak('попробуй сказать еще раз')
+            speak('попробуй сказать еще раз.')
             print("Ошибка в main.")
         
 
@@ -143,10 +181,14 @@ def execute_command(command):
                 goodbye()
             elif cmd == 'help':
                 help()
+            elif cmd == 'thank':
+                thank()
             elif cmd == 'weather':
-                weather()
+                weather('moscow', config.open_weather_token)
             elif cmd == 'open_browser':
                 open_browser()
+            elif cmd == 'open_sites':
+                open_sites()
             elif cmd == 'calc':
                 calc()
             elif cmd == 'convert':
